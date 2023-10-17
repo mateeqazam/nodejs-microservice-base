3bb95d88ba02566e66537d815be8e47afc077a13
Muhammad Ateeq Azam <m.ateeqazam@gmail.com>
2023-10-17 05:16:49 +0500

    update redis config

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 export const {
 	PORT = 5002,
-	NODE_ENV: ENV = 'development',
+	NODE_ENV: ENV = 'local-dev',
 	FILE_SIZE_LIMIT = '2mb',
-	SLACK_HOOK_URL = '',
-	ENCRYPTION_KEY = '',
+	SLACK_HOOK_URL
 } = process.env;
 
+export const IS_LOCAL = ENV === 'local-dev';
 export const IS_STAGING = ENV === 'staging';
 export const IS_PRODUCTION = ENV === 'production';
 export const IS_DEVELOPMENT = ENV === 'development';
-export const IS_LOCAL = ENV === 'local-dev' || (!IS_PRODUCTION && !IS_STAGING);
+export const IS_DEV = IS_LOCAL || IS_DEVELOPMENT || (!IS_PRODUCTION && !IS_STAGING);
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,14 @@
-import { IS_DEVELOPMENT } from './index';
+import { IS_LOCAL } from './index';
 
 const { REDIS_HOST = 'localhost', REDIS_PASSWORD, REDIS_PORT = 6379 } = process.env;
 
-const REDIS_CONFIG = IS_DEVELOPMENT
+const REDIS_CONFIG = IS_LOCAL
 	? {}
 	: {
 			url: `rediss://${REDIS_HOST}:${REDIS_PORT}`,
 			password: REDIS_PASSWORD,
-			// connectTimeout: 10000,
-			// reconnectStrategy: (retries) => Math.min(retries * 100, 3000),
+			connectTimeout: 10000,
+			reconnectStrategy: (retries) => Math.min(retries * 100, 3000),
 	  };
 
 export { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT };
